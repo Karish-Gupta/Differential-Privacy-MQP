6af7398269c3ca2ccaaddca81b08adca68622a73
Alex Li <alexlibaobao@gmail.com>
2025-09-03 15:37:30 -0400

    update file naming for nodp .out

--- DP_Fine-Tuning/flashDP/run_flashnodp.sh
+++ DP_Fine-Tuning/flashDP/run_flashnodp.sh
@@ -3,8 +3,8 @@
 #SBATCH -n 1                          # total number of tasks
 #SBATCH --mem=32g                     # allocate 32 GB of memory
 #SBATCH -J "run_noDP"              # name of the job
-#SBATCH -o flashdp_run_%j.out         # name of the output file
-#SBATCH -e flashdp_run_%j.err         # name of the error file
+#SBATCH -o flashnodp_run_%j.out         # name of the output file
+#SBATCH -e flashnodp_run_%j.err         # name of the error file
 #SBATCH -p short                      # partition to submit to
 #SBATCH -t 01:00:00                   # time limit of 1 hour
 #SBATCH --gres=gpu:H200:2             # request 2 H200 GPU
