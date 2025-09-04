a76a0a016c47fa3492d4fd609e1a6046a9f30588
Matthew Alex <matthewalex2005@gmail.com>
2025-09-04 18:27:56 -0400

    im silly

--- Data_DP/semanticParaphrasing/run_semantic.sh
+++ Data_DP/semanticParaphrasing/run_semantic.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1                          # allocate 1 compute node
 #SBATCH -n 1                          # total number of tasks
 #SBATCH --mem=32g                     # allocate 32 GB of memory
-#SBATCH -J "run_fastDP"              # name of the job
-#SBATCH -o fastdp_run_%j.out         # name of the output file
-#SBATCH -e fastdp_run_%j.err         # name of the error file
+#SBATCH -J "run_paraphrase"              # name of the job
+#SBATCH -o paraphrase_run_%j.out         # name of the output file
+#SBATCH -e paraphrase_run_%j.err         # name of the error file
 #SBATCH -p short                      # partition to submit to
 #SBATCH -t 02:00:00                   # time limit of 2 hours
 #SBATCH --gres=gpu:H200:1             # request 1 H200 GPU
@@ -12,17 +12,17 @@
 module load python/3.10.2/mqmlxcf
 module load cuda/12.4.0/3mdaov5
 
-python -m env
+python3 -m env
 source env/bin/activate
 
-pip install -U "huggingface_hub[cli]"
-pip install --upgrade pip
-pip install numpy
-pip install torch
-pip install transformers
-pip install datasets
-pip install tqdm
-pip install scikit-learn
-pip install accelerate
+pip3 install -U "huggingface_hub[cli]"
+pip3 install --upgrade pip
+pip3 install numpy
+pip3 install torch
+pip3 install transformers
+pip3 install datasets
+pip3 install tqdm
+pip3 install scikit-learn
+pip3 install accelerate
 
-python paraphraser.py
+python3 paraphraser.py
