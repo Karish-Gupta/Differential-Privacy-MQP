3995c9500753943b9acf437816472ca2641f30c8
Karish-Gupta <karishgupta2003@gmail.com>
2025-09-16 12:05:20 -0400

    renamed shell file output

--- DP_Fine-Tuning/baseline/run_baseline.sh
+++ DP_Fine-Tuning/baseline/run_baseline.sh
@@ -2,11 +2,11 @@
 #SBATCH -N 1                          # allocate 1 compute node
 #SBATCH -n 1                          # total number of tasks
 #SBATCH --mem=32g                     # allocate 32 GB of memory
-#SBATCH -J "run_flashDP"              # name of the job
-#SBATCH -o flashdp_run_%j.out         # name of the output file
-#SBATCH -e flashdp_run_%j.err         # name of the error file
+#SBATCH -J "run_baseline"              # name of the job
+#SBATCH -o baseline_run_%j.out         # name of the output file
+#SBATCH -e baseline_run_%j.err         # name of the error file
 #SBATCH -p short                      # partition to submit to
-#SBATCH -t 012:00:00                   # time limit of 12 hours
+#SBATCH -t 12:00:00                   # time limit of 12 hours
 #SBATCH --gres=gpu:H100:1             # request 1 H100 GPU
 
 module load python/3.11.10
